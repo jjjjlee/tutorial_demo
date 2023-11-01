09b2330380b4a2cab617eb4331564f66a678a8a7
jjjjlee <jamesjoy13579@gmail.com>
2023-11-01 22:07:38 +0800

    Initialize the deom

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style = {styles.title}>Cowork Demo</Text>
+      <Image style = {styles.image} source = {require('./assets/cowork_demo.jpg')} />
+      <Text style = {styles.description}>BOBO is the best!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#CAF1DE',
+    alignItems: 'center'
   },
+  title:{
+    marginTop: 150,
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 40
+  },
+  image: {
+    width:500,
+    height:500,
+    marginBottom :100
+  },
+  description: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#333',
+  }
 });
