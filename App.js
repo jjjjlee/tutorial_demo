6fdfb621686d8902a68e460e6a9283b17defd7df
jjjjlee <jamesjoy13579@gmail.com>
2023-11-02 01:34:54 +0800

    Change to tictactoe

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const winner = calculateWinner(board);
+  const status = winner ? `Winner: ${winner}` : `Next Player: ${isXNext ? 'X' : 'O'}`;
+
+  const renderSquare = (i) => (
+    <TouchableOpacity style={styles.square} onPress={() => handleSquareClick(i)}>
+      <Text style={styles.squareText}>{board[i]}</Text>
+    </TouchableOpacity>
+  );
+
+  const handleSquareClick = (i) => {
+    if (board[i] || winner) {
+      return;
+    }
+    const newBoard = board.slice();
+    newBoard[i] = isXNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style = {styles.title}>Cowork Demo</Text>
-      <Image style = {styles.image} source = {require('./assets/cowork_demo.jpg')} />
-      <Text style = {styles.description}>BOBO is the best!!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.status}>{status}</Text>
+      <View style={styles.board}>
+        <View style={styles.row}>
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </View>
+        <View style={styles.row}>
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </View>
+        <View style={styles.row}>
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,24 +51,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#CAF1DE',
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  title:{
-    marginTop: 150,
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#333',
-    marginBottom: 40
+  status: {
+    marginBottom: 10,
   },
-  image: {
-    width:500,
-    height:500,
-    marginBottom :100
+  board: {
+    flexDirection: 'column',
   },
-  description: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#333',
-  }
+  row: {
+    flexDirection: 'row',
+  },
+  square: {
+    width: 60,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  squareText: {
+    fontSize: 24,
+  },
 });
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
